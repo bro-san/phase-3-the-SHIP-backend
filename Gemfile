29cbbc379b4cb6ad0a99a10cc75f5427a82fd472
Brody S <brody.sanford@gmail.com>
2022-07-15 12:09:16 -0400

    trying to get around sqlite issue

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem "sinatra-activerecord", "~> 2.0"
 gem "rake", "~> 13.0"
 
 # Provides functionality to interact with a SQLite3 database
-gem "sqlite3", "~> 1.4"
+# gem "sqlite3", "~> 1.4"
 
 # Require all files in a folder
 gem "require_all", "~> 3.0"
@@ -43,6 +43,15 @@ group :development do
   # Automatically reload when there are changes
   # https://github.com/alexch/rerun
   gem "rerun"
+end
+
+group :development do
+   gem 'sqlite3'
+end
+
+group :production do
+   gem 'pg'
+   gem 'activerecord-postgresql-adapter'
 end
 
 # These gems will only be used when we are running tests
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,23 @@
+configure :development do
+    ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  )
+  end
+  
+  configure :production do
+    db = URI.parse(ENV['HEROKU_POSTGRESQL_COBALT_URL'] || 'postgres://localhost/mydb')
+  
+    ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
+  end
+
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
 ENV['RACK_ENV'] ||= "development"
